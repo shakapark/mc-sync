96ef5f764584c2187e783572f4312d0c922a6446
cvando <clement.vandoolaeghe@gmail.com>
2018-03-01 18:14:39 +0100

    sync update INIT

--- sync.sh
+++ sync.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+if [ $INIT = true ]
+then
+  echo "Starting initialization"
+  mc mirror --force --remove $SRC $DST > /dev/null 2>&1
+  echo "Initialization done"
+else
   while [ true ]; do
     echo "Starting synchronization"
-    mc mirror --force --remove $IN $OUT > /dev/null 2>&1
-    echo "Synchronization done"
-    sleep 30
+    mc mirror --force --remove $SRC $DST > /dev/null 2>&1
+    echo "Synchronization done sleep 60"
+    sleep 60
   done
+fi
