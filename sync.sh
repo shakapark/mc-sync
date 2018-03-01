7d8a15ce727546093b399d434cfb00f2f7556dce
cvando <clement.vandoolaeghe@gmail.com>
2018-03-01 18:05:21 +0100

    sync update IN OUT

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
   while [ true ]; do
     echo "Starting synchronization"
-    mc mirror --force --remove $localdir $bucket > /dev/null 2>&1
+    mc mirror --force --remove $IN $OUT > /dev/null 2>&1
     echo "Synchronization done"
     sleep 30
   done
