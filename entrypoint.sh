4e2d4b734c61e1033c132adc778a02ba3a81dc56
Shakapark <shakapark1994@gmail.com>
2018-03-08 12:32:56 +0100

    Update backup function

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,19 +3,26 @@
 function backup() {
   echo "Starting Backup"
 
+  DATE=$(date -d "$RETENTION days ago" +"%d-%m-%Y")
+  mc rm --recursive --force $DST/$DATE
+
   DATE=$(date +"%d-%m-%Y")
   mc mb $DST/$DATE
+  echo $?
 
   BUCKETS=$(mc --json ls $SRC | grep -Eo '"key":.*?[^\\]",'|awk -F':' '{print $2}' | cut -d \" -f2 | cut -d / -f1 | tr " " "\n")
   for BUCKET in $BUCKETS
   do
     mc cp -r $SRC/$BUCKET $DST/$DATE
+    if [ $? != 0 ]
+    then
+      exit 1
+    fi
   done
 
-  DATE=$(date -d "$RETENTION days ago" +"%d-%m-%Y")
-  mc rm --recursive --force $DST/$DATE
-
   echo "Backup Done"
+
+  exit 0
 }
 
 function init() {
